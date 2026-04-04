f6aeddcab75ecb09802211bfec3b7a1cb73907e3
cris <54333719+andreedev@users.noreply.github.com>
2026-04-04 14:32:45 +0200

    Add user table creation script to database initialization

--- db/script.sql
+++ db/script.sql
@@ -1,1 +1,12 @@
 /* Script de base de datos */
+
+CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL,
+    postal_code VARCHAR(20),
+    profile_photo_key VARCHAR(255) comment 'Clave del archivo de la foto de perfil en S3',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
