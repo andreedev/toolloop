b123a75c1282cdc45690154df74d8899a96e7133
maria <mariaqingxuan@gmail.com>
2026-04-08 21:56:21 +0200

    eliminacion del proyecto y volver a crearlo sin tests

--- toolloop-front/src/app/app.ts
+++ toolloop-front/src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
-import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer,Header],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
