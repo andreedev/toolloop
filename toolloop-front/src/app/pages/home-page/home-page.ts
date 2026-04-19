1c462fd637c3b16dce8fb8e8afba09df20449f6f
maria <mariaqingxuan@gmail.com>
2026-04-19 02:48:21 +0200

    front home casi terminado

--- toolloop-front/src/app/pages/home-page/home-page.ts
+++ toolloop-front/src/app/pages/home-page/home-page.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faLeaf, faArrowRight,faLocationDot,faDollarSign,faUsers,faHashtag, faHeart, faShield} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-home-page',
-    imports: [RouterLink],
+    imports: [RouterLink, FontAwesomeModule],
     templateUrl: './home-page.html',
     styleUrl: './home-page.scss',
 })
-export class HomePage {}
+export class HomePage {
+    faLeaf = faLeaf;
+    faArrowRight = faArrowRight;
+    faLocationDot = faLocationDot;
+    faDollarSign = faDollarSign;
+    faUsers = faUsers;
+    faHashtag = faHashtag;
+    faHeart = faHeart;
+    faShield = faShield;
+}
