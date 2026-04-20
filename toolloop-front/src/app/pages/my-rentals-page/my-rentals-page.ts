9b563ece15c6590dd37e3bc5589d71c3442f77c9
cris <54333719+andreedev@users.noreply.github.com>
2026-04-20 06:19:46 +0200

    cambios

--- toolloop-front/src/app/pages/my-rentals-page/my-rentals-page.ts
+++ toolloop-front/src/app/pages/my-rentals-page/my-rentals-page.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faKey, faShieldCat} from '@fortawesome/free-solid-svg-icons';
+import {faClock, faStar} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
     selector: 'app-my-rentals-page',
-    imports: [],
+    imports: [RouterLink, CommonModule, FontAwesomeModule],
     templateUrl: './my-rentals-page.html',
     styleUrl: './my-rentals-page.scss',
 })
-export class MyRentalsPage {}
+export class MyRentalsPage {
+    faKey = faKey;
+    faClock = faClock;
+    faShieldCat = faShieldCat;
+    faStar = faStar;
+}
