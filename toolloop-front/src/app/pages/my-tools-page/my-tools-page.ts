d5708b7c4d2ad13bd9b3bba67fcc2105db6d51e4
maria <mariaqingxuan@gmail.com>
2026-04-20 03:01:17 +0200

    empezando mis herramientas

--- toolloop-front/src/app/pages/my-tools-page/my-tools-page.ts
+++ toolloop-front/src/app/pages/my-tools-page/my-tools-page.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {faWrench, faArrowTrendUp, faPencil,faTrashCan, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-my-tools-page',
-    imports: [],
+    imports: [RouterLink, FontAwesomeModule],
     templateUrl: './my-tools-page.html',
     styleUrl: './my-tools-page.scss',
 })
-export class MyToolsPage {}
+export class MyToolsPage {
+    faWrench = faWrench;
+    faArrowTrendUp = faArrowTrendUp;
+    faPencil = faPencil;
+    faTrashCan = faTrashCan;
+    faPlus = faPlus;
+
+
+}
