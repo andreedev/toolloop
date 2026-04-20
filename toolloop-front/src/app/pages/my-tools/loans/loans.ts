e8c53b4e1dd6276be99e907002dc961eb4779428
maria <mariaqingxuan@gmail.com>
2026-04-20 05:45:34 +0200

    mis herramientas pt2 empezado

--- toolloop-front/src/app/pages/my-tools/loans/loans.ts
+++ toolloop-front/src/app/pages/my-tools/loans/loans.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {faCheck, faCross, faCalendar, faShield, faStar, faHashtag } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-loans',
-    imports: [],
+    imports: [RouterLink, FontAwesomeModule],
     templateUrl: './loans.html',
     styleUrl: './loans.scss',
 })
-export class Loans {}
+export class Loans {
+    faCheck = faCheck;
+    faCross = faCross;
+    faCalendar = faCalendar;
+    faShield = faShield;
+    faStar = faStar;
+    faHashtag = faHashtag;
+}
