573f621592da24b6b8595bf3da6a7f91298d3a0a
maria <mariaqingxuan@gmail.com>
2026-04-27 03:46:52 +0200

    empezado añadir herramientas

--- toolloop-front/src/app/pages/add-tool-page/add-tool-page.ts
+++ toolloop-front/src/app/pages/add-tool-page/add-tool-page.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faArrowRight,faArrowLeft,faEuroSign, faArrowUpFromBracket, faCross, faCheck} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-add-tool-page',
-    imports: [],
+    imports: [RouterLink, FontAwesomeModule],
     templateUrl: './add-tool-page.html',
     styleUrl: './add-tool-page.scss',
 })
-export class AddToolPage {}
+export class AddToolPage {
+
+    faArrowRight = faArrowRight;
+    faArrowLeft = faArrowLeft;
+    faEuroSign = faEuroSign;
+    faArrowUpFromBracket = faArrowUpFromBracket;
+    faCross = faCross;
+    faCheck = faCheck;
+
+}
