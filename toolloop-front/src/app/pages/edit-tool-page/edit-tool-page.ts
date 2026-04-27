d18783a459e921b2bc08cf7015953b6a94774baa
maria <mariaqingxuan@gmail.com>
2026-04-27 13:56:17 +0200

    editar herramienta casi terminado, solo falta calendario y unas cosillas

--- toolloop-front/src/app/pages/edit-tool-page/edit-tool-page.ts
+++ toolloop-front/src/app/pages/edit-tool-page/edit-tool-page.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faArrowRight,faArrowLeft,faEuroSign, faArrowUpFromBracket, faX, faCheck, faCircle, faSquare} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-    selector: 'app-edit-tool-page',
-    imports: [],
+    selector: 'app-add-tool-page',
+    imports: [RouterLink, FontAwesomeModule],
     templateUrl: './edit-tool-page.html',
     styleUrl: './edit-tool-page.scss',
 })
-export class EditToolPage {}
+export class AddToolPage {
+
+    faArrowRight = faArrowRight;
+    faArrowLeft = faArrowLeft;
+    faEuroSign = faEuroSign;
+    faArrowUpFromBracket = faArrowUpFromBracket;
+    faX = faX;
+    faCheck = faCheck;
+    faCircle = faCircle;
+    faSquare = faSquare;
+
+}
