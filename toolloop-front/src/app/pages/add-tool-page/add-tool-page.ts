26b951263c910bc6d784cfb296ad1ce9e7284eab
maria <mariaqingxuan@gmail.com>
2026-04-27 13:39:04 +0200

    añadir herramienta terminado, solo falta calendario y unas cosillas

--- toolloop-front/src/app/pages/add-tool-page/add-tool-page.ts
+++ toolloop-front/src/app/pages/add-tool-page/add-tool-page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {faArrowRight,faArrowLeft,faEuroSign, faArrowUpFromBracket, faCross, faCheck} from '@fortawesome/free-solid-svg-icons';
+import {faArrowRight,faArrowLeft,faEuroSign, faArrowUpFromBracket, faX, faCheck, faCircle, faSquare} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-add-tool-page',
@@ -15,7 +15,9 @@ export class AddToolPage {
     faArrowLeft = faArrowLeft;
     faEuroSign = faEuroSign;
     faArrowUpFromBracket = faArrowUpFromBracket;
-    faCross = faCross;
+    faX = faX;
     faCheck = faCheck;
+    faCircle = faCircle;
+    faSquare = faSquare;
 
 }
