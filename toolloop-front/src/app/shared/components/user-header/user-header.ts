739480d972486ec255fcaf99cf1a4529a9947add
andree <54333719+andreedev@users.noreply.github.com>
2026-05-02 00:42:57 +0200

    feat: enhance user header with responsive menu and dialog functionality

--- toolloop-front/src/app/shared/components/user-header/user-header.ts
+++ toolloop-front/src/app/shared/components/user-header/user-header.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { faBell, faComment } from '@fortawesome/free-regular-svg-icons';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
     selector: 'user-header',
-    imports: [RouterLink, RouterLinkActive ,FontAwesomeModule],
+    imports: [RouterLink, RouterLinkActive, FontAwesomeModule, DialogModule],
     templateUrl: './user-header.html',
     styleUrl: './user-header.scss',
 })
@@ -14,5 +15,12 @@ export class UserHeader {
     faUser = faUser;
     faBell = faBell;
     faComment = faComment;
+    faBars = faBars;
+    faXmark = faXmark;
 
+    menuOpen = false;
+
+    closeMenu(): void {
+        this.menuOpen = false;
+    }
 }
