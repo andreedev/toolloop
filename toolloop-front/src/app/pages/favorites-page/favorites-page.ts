f3c1078f7099180b10ff82f06892c6b817471cc8
maria <mariaqingxuan@gmail.com>
2026-05-04 00:39:06 +0200

    settings-page hecho y favoritos

--- toolloop-front/src/app/pages/favorites-page/favorites-page.ts
+++ toolloop-front/src/app/pages/favorites-page/favorites-page.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart, faStar, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-favorites-page',
-    imports: [],
+    imports: [FontAwesomeModule, RouterLink],
     templateUrl: './favorites-page.html',
     styleUrl: './favorites-page.scss',
 })
-export class FavoritesPage {}
+export class FavoritesPage {
+    public faHeart = faHeart;
+    public faStar = faStar;
+    public faLocationDot = faLocationDot;
+}
--- toolloop-front/src/app/pages/settings-page/settings-page.ts
+++ toolloop-front/src/app/pages/settings-page/settings-page.ts
@@ -4,10 +4,15 @@ import { UserDataService } from '../../core/services/data/user.data.service';
 import { GeneralDataService } from '../../core/services/data/general.data.service';
 import { Router } from '@angular/router';
 import { UserApiService } from '../../core/services/api/user.api.service';
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCamera, faUser, faEnvelope, faLocationDot, faLock, faBell, faTrashCan, faArrowRightFromBracket, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-settings-page',
-    imports: [],
+    imports: [FaIconComponent, FontAwesomeModule, ToggleSwitchModule, RouterLink],
     templateUrl: './settings-page.html',
     styleUrl: './settings-page.scss',
 })
@@ -25,4 +30,14 @@ export class SettingsPage {
         this.authDataService.deleteSession();
         void this.router.navigate(['/']);
     }
+
+    public faCamera = faCamera;
+    public faUser = faUser;
+    public faEnvelope = faEnvelope;
+    public faLocationDot = faLocationDot
+    public faLock = faLock;
+    public faBell = faBell
+    public faTrashCan = faTrashCan;
+    public faArrowRightFromBracket = faArrowRightFromBracket;
+    public faAngleRight = faAngleRight;
 }
