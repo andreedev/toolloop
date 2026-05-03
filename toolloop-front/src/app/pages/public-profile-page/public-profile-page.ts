01cb7f48c3471046417a1f148bb2c9319387a536
maria <mariaqingxuan@gmail.com>
2026-05-03 05:14:36 +0200

    Public profile finished

--- toolloop-front/src/app/pages/public-profile-page/public-profile-page.ts
+++ toolloop-front/src/app/pages/public-profile-page/public-profile-page.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {faStar, faCalendar, faAward, faHouse, faWrench, faUser} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
     selector: 'app-public-profile-page',
-    imports: [],
+    imports: [FontAwesomeModule, RouterLink],
     templateUrl: './public-profile-page.html',
     styleUrl: './public-profile-page.scss',
 })
-export class PublicProfilePage {}
+export class PublicProfilePage {
+    public faStar = faStar;
+    public faCalendar = faCalendar;
+    public faAward = faAward;
+    public faHouse = faHouse;
+    public faWrench = faWrench;
+    public faUser = faUser;
+}
