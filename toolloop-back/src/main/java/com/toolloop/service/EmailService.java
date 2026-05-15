8e8de8d092493d62e5af5a78e0276ef7285b2a76
andree <54333719+andreedev@users.noreply.github.com>
2026-05-16 01:52:35 +0200

    feat: replace Mailjet with Quarkus Mailer for email sending functionality

--- toolloop-back/src/main/java/com/toolloop/service/EmailService.java
+++ toolloop-back/src/main/java/com/toolloop/service/EmailService.java
@@ -1,75 +1,28 @@
 package com.toolloop.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.mailjet.client.ClientOptions;
-import com.mailjet.client.MailjetClient;
-import com.mailjet.client.errors.MailjetException;
-import com.mailjet.client.transactional.SendContact;
-import com.mailjet.client.transactional.TrackClicks;
-import com.mailjet.client.transactional.TrackOpens;
-import com.mailjet.client.transactional.TransactionalEmail;
-import com.mailjet.client.transactional.SendEmailsRequest;
-import com.mailjet.client.transactional.response.SendEmailsResponse;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
 
 @Slf4j
 @ApplicationScoped
 public class EmailService {
 
-    @ConfigProperty(name = "mailjet.apiKeyPublic")
-    String apiKeyPublic;
-
-    @ConfigProperty(name = "mailjet.apiKeyPrivate")
-    String apiKeyPrivate;
-
-    @ConfigProperty(name = "app.mail.sender.email")
-    String senderEmail;
-
-    @ConfigProperty(name = "app.mail.sender.name")
-    String senderName;
+    @Inject
+    Mailer mailer;
 
     public void sendEmail(String toEmail, String toName, String subject, String htmlContent) {
         try {
-            ClientOptions options = ClientOptions.builder()
-                    .apiKey(apiKeyPublic)
-                    .apiSecretKey(apiKeyPrivate)
-                    .build();
-
-            MailjetClient client = new MailjetClient(options);
-
-            TransactionalEmail message = TransactionalEmail
-                    .builder()
-                    .from(new SendContact(senderEmail, senderName))
-                    .to(new SendContact(toEmail, toName))
-                    .htmlPart(htmlContent)
-                    .subject(subject)
-                    .trackOpens(TrackOpens.ENABLED)
-                    .trackClicks(TrackClicks.DISABLED)
-                    .build();
-
-            SendEmailsRequest request = SendEmailsRequest
-                    .builder()
-                    .message(message)
-                    .build();
-
-            SendEmailsResponse response = request.sendWith(client);
-
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.enable(SerializationFeature.INDENT_OUTPUT);
-            try {
-                String fullJson = mapper.writeValueAsString(response);
-                log.info("mailjet response details:\n{}", fullJson);
-            } catch (Exception e) {
-                log.error("could not serialize Mailjet response: {}", e.getMessage());
-            }
-
-            log.info("email sent to {}. Status: {}", toEmail, response.getMessages()[0].getStatus());
-
-        } catch (MailjetException e) {
+            mailer.send(
+                Mail.withHtml(toEmail, subject, htmlContent)
+                    .setTo(List.of(toName + " <" + toEmail + ">"))
+            );
+            log.info("email sent to {}", toEmail);
+        } catch (Exception e) {
             log.error("failed to send email to {}: {}", toEmail, e.getMessage());
         }
     }
