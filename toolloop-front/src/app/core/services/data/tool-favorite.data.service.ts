8b727fc4b228858e6d6bf10a461962cc8130799f
andree <54333719+andreedev@users.noreply.github.com>
2026-05-15 03:38:52 +0200

    change

--- toolloop-front/src/app/core/services/data/tool-favorite.data.service.ts
+++ toolloop-front/src/app/core/services/data/tool-favorite.data.service.ts
@@ -1,15 +1,12 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ToolFavoriteApiService } from '../api/tool-favorite.api.service';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { HttpResponseBody } from '../../models/dto/http-response-body';
-import { ToolFavorite } from '../../models/entity/tool-favorite';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ToolFavoriteDataService {
     private toolFavoriteApiService = inject(ToolFavoriteApiService);
-
 
     async toggleFavorite(toolId: number): Promise<boolean> {
         const response = await this.toolFavoriteApiService.toggleFavorite(toolId);
