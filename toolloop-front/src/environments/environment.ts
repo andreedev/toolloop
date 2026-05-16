3515f47a8dc47f926941be308ae6500a4c0002be
andree <54333719+andreedev@users.noreply.github.com>
2026-05-16 03:47:08 +0200

    change

--- toolloop-front/src/environments/environment.ts
+++ toolloop-front/src/environments/environment.ts
@@ -3,10 +3,10 @@ export const environment = {
     production: false,
 
     // Dominio base de la API
-    // apiDomain: 'http://localhost:8080/toolloopapi/',
-    // wsDomain: 'ws://localhost:8080/',
+    apiDomain: 'http://localhost:8080/toolloopapi/',
+    wsDomain: 'ws://localhost:8080/',
 
-    apiDomain: 'http://ec2-13-222-187-246.compute-1.amazonaws.com:8080/toolloopapi/',
-    wsDomain: 'ws://ec2-13-222-187-246.compute-1.amazonaws.com:8080/',
+    // apiDomain: 'http://ec2-13-222-187-246.compute-1.amazonaws.com:8080/toolloopapi/',
+    // wsDomain: 'ws://ec2-13-222-187-246.compute-1.amazonaws.com:8080/',
 
 };
