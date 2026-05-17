2827055afa4f71f1645e0fdb3468df29c6eda7d8
andree <54333719+andreedev@users.noreply.github.com>
2026-05-17 19:35:14 +0200

    fix: update EC2 host and API domains to the correct address

--- toolloop-front/src/environments/environment.prod.ts
+++ toolloop-front/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
     production: true,
 
     // Dominio base de la API
-    apiDomain: 'http://ec2-13-222-187-246.compute-1.amazonaws.com:8080/toolloopapi/',
-    wsDomain: 'ws://ec2-13-222-187-246.compute-1.amazonaws.com:8080/',
+    apiDomain: 'http://ec2-54-166-27-151.compute-1.amazonaws.com:8080/toolloopapi/',
+    wsDomain: 'ws://ec2-54-166-27-151.compute-1.amazonaws.com:8080/',
 
 };
--- toolloop-front/src/environments/environment.ts
+++ toolloop-front/src/environments/environment.ts
@@ -4,9 +4,6 @@ export const environment = {
 
     // Dominio base de la API
     apiDomain: 'http://localhost:8080/toolloopapi/',
-    wsDomain: 'ws://localhost:8080/',
-
-    // apiDomain: 'http://ec2-13-222-187-246.compute-1.amazonaws.com:8080/toolloopapi/',
-    // wsDomain: 'ws://ec2-13-222-187-246.compute-1.amazonaws.com:8080/',
+    wsDomain: 'ws://localhost:8080/'
 
 };
