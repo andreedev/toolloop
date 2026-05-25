63858c485bc4f1209c70db02e633f487ba5764e7
cris <54333719+andreedev@users.noreply.github.com>
2026-05-25 21:37:39 +0200

    style: update button layout and padding in my rentals page

--- toolloop-front/src/app/pages/map-page/map-page.ts
+++ toolloop-front/src/app/pages/map-page/map-page.ts
@@ -54,9 +54,10 @@ export class MapPage implements OnInit, OnDestroy {
 
     mapOptions: L.MapOptions = {
         layers: [
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 18,
-                attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+            L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+                maxZoom: 20,
+                subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+                attribution: '&copy; Google Maps',
             }),
         ],
         zoom: 14,
